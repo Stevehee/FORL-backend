33ab363c91e8e545abde12a11c9628fae48ab183
Steve He <stevehe@g.ucla.edu>
2023-10-17 15:02:56 -0700

    init spring boot

--- FOIL/src/main/java/com/FOIL/FoilApplication.java
+++ FOIL/src/main/java/com/FOIL/FoilApplication.java
@@ -1,4 +1,4 @@
-package com.steve.FOIL;
+package com.FOIL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- FOIL/src/test/java/com/FOIL/FoilApplicationTests.java
+++ FOIL/src/test/java/com/FOIL/FoilApplicationTests.java
@@ -1,4 +1,4 @@
-package com.steve.FOIL;
+package com.FOIL;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
